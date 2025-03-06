5821894381a000e59a505ac9c503425d9ad1e941
Newton Muhindi <muhindinewton@gmail.com>
2025-03-06 15:30:45 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavouriteCustomer = 'Kamau';
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+    
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+   bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavouriteCustomer = 'Biggy';
+}
+console.log (bestCustomer)
